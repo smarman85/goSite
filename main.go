0376c8ddab60e7d20e7c00ab9036940a5e064749
Sean <seanmarman@gmail.com>
2020-04-13 00:36:57 -0700

    first

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  //"fmt"
+  "net/http"
+  "html/template"
+  "github.com/gorilla/mux"
+)
+
+func staticHTML(htmlTemplate string, resp http.ResponseWriter) {
+  //templ := template.Must(template.ParseFiles("tmpl/" + htmlTemplate + ".html"))
+  templ := template.Must(template.ParseFiles("static/tmpl/" + htmlTemplate + ".html"))
+  templ.Execute(resp, nil)
+
+}
+
+func main() {
+  router:= mux.NewRouter()
+
+  router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    staticHTML("index", w)
+  })
+
+  router.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+    //fmt.Fprintf(w, "About me")
+    staticHTML("about", w)
+  })
+
+  router.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request){
+    staticHTML("projects", w)
+  })
+
+  router.HandleFunc("/resume", func(w http.ResponseWriter, r *http.Request){
+    staticHTML("resume", w)
+  })
+
+  //http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+  fs := http.FileServer(http.Dir("./static"))
+  http.Handle("/", fs)
+  http.ListenAndServe(":8088", router)
+}
