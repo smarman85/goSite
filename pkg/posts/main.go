a9b5f908ae44797e47eb5fa431b94add23bfe914
Sean <seanmarman@gmail.com>
2021-03-13 13:32:45 -0800

    more content ideas

--- pkg/posts/main.go
+++ pkg/posts/main.go
@@ -7,7 +7,7 @@ var Projects map[string][]string = map[string][]string{
     "k8s-svc": []string{"K8s Service Accounts", "Kubernetes Service accounts and how they grant access."},
     "helm": []string{"Helm Manifests", "Building K8s manifests with Helm3"},
     "terraform": []string{"Terraforming AWS Resources", "Building out your AWS infrastructure with Terraform"},
-    "test2": []string{"test2 header", "quick desc 3"},
+    "docker": []string{"Dockerfile Best Practices", "Setting up minimal and secure dockerfiles"},
     "traefik": []string{"Local developement with Traefik", "Use traefik to simplify local development"},
     "post1": []string{"Lorem Ipsume", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
 }
