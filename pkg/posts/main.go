e9b77c167cb98177e865704741cf1f95a66c20cc
Sean <seanmarman@gmail.com>
2021-03-13 12:37:34 -0800

    wip - adding content to site

--- pkg/posts/main.go
+++ pkg/posts/main.go
@@ -1,9 +1,9 @@
 package posts
 
 var Projects map[string][]string = map[string][]string{
-    "test": []string{"test header", "quick desc"},
-    "app-of-apps": []string{"Argocd Applications", "Argocd app of apps definition"},
-    "test1": []string{"test1 header", "quick desc 1"},
+    "go-api": []string{"Golang API", "Simple Api using gorilla/mux in golang."},
+    "app-of-apps": []string{"Argocd Applications", "Argocd app of apps definition."},
+    "github-actions": []string{"Github actions", "Automating workflows with github actions."},
     "test2": []string{"test2 header", "quick desc 2"},
     "test3": []string{"test3 header", "quick desc 3"},
     "test4": []string{"test4 header", "quick desc 4"},
